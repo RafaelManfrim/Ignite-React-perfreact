f1fd22615f5cb239b6d5da768da81a55b5662da9
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2022-01-24 18:55:50 -0300

    Utilizando memo

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,13 +1,18 @@
+import { memo } from "react";
 import { Product } from "../types/Product";
 
 interface ProductItemProps {
     product: Product
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
     return (
         <div>
             {product.title} - <strong>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(product.price)}</strong>
         </div>
     )
 }
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product)
+})
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -16,3 +16,16 @@ export function SearchResults({ results }: SearchResultsProps) {
         </div>
     )
 }
+
+// Comportamento padrão React:
+// Processo de renderização
+// 1. Criar uma nova versão do componente
+// 2. Comparar com a versão anterior
+// 3. Se houverem alterações, vai atualizar o que alterou
+
+
+// memo - usar quando
+// 1. Componentes funcionais puros ( sem funcionalidade própria, apenas exibir informações, criados deixar o código mais limpo e separado )
+// 2. Quando o componente renderiza muitas vezes
+// 3. Quando o componente renderiza com as mesmas props
+// 4. Componentes de tamanho médio para grande
